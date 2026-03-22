c64267427582b8e6f331237685cc992af5ed949b
shrimp-200516 <bunauea1981@gmail.com>
2026-03-22 13:03:54 +0700

    update UI

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.set_page_config(
     page_title="IS Project — ML & Neural Network",
-    page_icon="🤖",
+    page_icon="",
     layout="wide",
     initial_sidebar_state="expanded"
 )
